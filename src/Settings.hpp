5ecfce65cb0f0db33a317bf57c3bdd634e0b5018
Gagarine Yaikhom <gyaikhom@gmail.com>
2014-01-19 15:25:08 +0000

    Added data structure for maintaining an ordered set of landmarks,

--- src/Settings.hpp
+++ src/Settings.hpp
@@ -29,6 +29,9 @@ const int NAME_REPEATED = -2;
 const int FAILED_TO_OPEN_FILE = -3;
 const int INVALID_NUM_LMARKS = -4;
 const int INVALID_EDGE_WEIGHT = -5;
+const int REPEATED_ITEM_IN_SET = -6;
+const int LMARK_NOT_FOUND = -7;
+const int NOT_ENOUGH_ELEMENTS = -8;
 
 #endif /* SETTINGS_HPP */
 
